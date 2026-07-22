198be7e87ffc7894281cc960c70b6066e8282a49
Noah Tigner <noahzanetigner@gmail.com>
2026-07-21 22:14:20 -0600

    Time execution & count matches

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"time"
 
 	// trie "github.com/noahtigner/go-autocomplete/1_trie"
 	// trieOrdered "github.com/noahtigner/go-autocomplete/2_trie_ordered"
@@ -30,6 +31,8 @@ func readFileIntoMemory(filename string) ([]models.Product, error) {
 }
 
 func main() {
+	start := time.Now()
+
 	args := os.Args[1:]
 	if len(args) < 1 {
 		fmt.Println("Error: missing 1 positional argument for the search term")
@@ -37,6 +40,10 @@ func main() {
 	}
 
 	query := args[0]
+	if len(query) == 0 {
+		fmt.Println("Error: the first positional argument must be a nonempty search term")
+		os.Exit(1)
+	}
 
 	products, err := readFileIntoMemory("./data/products.json")
 	if err != nil {
@@ -46,7 +53,10 @@ func main() {
 
 	matches := trieWholeWordsAnyPosition.Search(products, query)
 
-	for _, match := range matches {
+	for _, match := range matches[:10] {
 		fmt.Println(match)
 	}
+
+	duration := time.Since(start)
+	fmt.Printf("Found %d results in %s\n", len(matches), duration)
 }
