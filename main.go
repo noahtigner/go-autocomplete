7d2d7617b29a1e45b2c9668c8423261cc4c41676
Noah Tigner <noahzanetigner@gmail.com>
2026-08-19 22:00:40 -0600

    Beginning of an HTTP Server

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"strconv"
 	"time"
@@ -10,7 +11,43 @@ import (
 	autocomplete "github.com/noahtigner/go-autocomplete/autocomplete"
 )
 
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func search(w http.ResponseWriter, req *http.Request, idx *autocomplete.Index, query autocomplete.SearchParams) {
+	ctx := req.Context()
+
+	select {
+	case <-ctx.Done():
+		err := ctx.Err()
+		fmt.Println("Server:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	default:
+		searchStart := time.Now()
+		results := idx.Search(query)
+		searchDuration := time.Since(searchStart)
+		for _, match := range results.Movies {
+			yearStr := "unknown"
+			if match.Year != nil {
+				yearStr = strconv.Itoa(*match.Year)
+			}
+			fmt.Fprintf(w, "\t%s (%s)\t[%d votes -> %f]\n", match.PrimaryTitle, yearStr, match.NumVotes, match.BayesianRating())
+		}
+		fmt.Fprintf(w, "Found %d results in %.2fs\n", results.Total, searchDuration.Seconds())
+	}
+}
+
 func main() {
+
 	ioStart := time.Now()
 
 	flag.Parse()
@@ -35,19 +72,11 @@ func main() {
 	ioDuration := time.Since(ioStart)
 	fmt.Printf("Processed %d records in %.2fs\n", processedCount, ioDuration.Seconds())
 
-	searchStart := time.Now()
+	http.HandleFunc("/search", func(w http.ResponseWriter, req *http.Request) {
+		search(w, req, &index, query)
+	})
 
-	results := index.Search(query)
-
-	searchDuration := time.Since(searchStart)
-
-	for _, match := range results.Movies {
-		yearStr := "unknown"
-		if match.Year != nil {
-			yearStr = strconv.Itoa(*match.Year)
-		}
-		fmt.Printf("\t%s (%s)\t[%d votes -> %f]\n", match.PrimaryTitle, yearStr, match.NumVotes, match.BayesianRating())
-	}
-
-	fmt.Printf("Found %d results in %.2fs\n", results.Total, searchDuration.Seconds())
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+	http.ListenAndServe(":8090", nil)
 }
